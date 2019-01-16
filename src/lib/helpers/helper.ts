84a19c4785760872bd65ab3351b20cbcc88707dc
Bryan Friend <bryancfriend@gmail.com>
2019-01-16 14:00:25 -0700

    Remove old methods

--- src/lib/helpers/helper.ts
+++ src/lib/helpers/helper.ts
@@ -1,20 +1,3 @@
-/**
- * Using the given file system, creates a file representing a new custom metadata type.
- * @param fs
- * @param devname
- * @param label
- * @param plurallabel
- * @param visibility
- */
-export async function writeTypeFile(fs, devName, objectXML) {
-    // appending __mdt if they did not pass it in. 
-    if ( devName.endsWith('__mdt') === false ) {
-        devName += '__mdt';
-    }
-    const outputFilePath = `${devName}/${devName}.object-meta.xml`;
-    await fs.mkdirp(`${devName}`);
-    await fs.writeFile(outputFilePath, objectXML);
-}
 
 /**
  * Using the given file system, creates a file representing a new record for the given custom metadata type
@@ -42,22 +25,6 @@ export function createRecord(fs, typename, recname, label, protection, varargs) 
 
 }
 
-/**
- * Using the given file system, creates a file representing a new field for the given custom metadata type
- * @param fs
- * @param fieldname
- * @param fieldXML
- */
-// /fields/{fieldAPI}.field-meta.xml
-export function writeFieldFile(fs,fieldName, fieldXML){
-    // appending __c if its not already there
-    if ( fieldName.endsWith('__c') === false ) {
-        fieldName += '__c';
-    }
-    const outputFilePath =`fields/${fieldName}.field-meta.xml`;
-    fs.mkdirp('fields');
-    fs.writeFile(outputFilePath,fieldXML);
-}
 
 function buildCustomFieldXml(map: object) {
     let ret = '';
