03adccd1afaace828b4de25bd015cf1a2240f412
Willie Ruemmele <willieruemmele@gmail.com>
2026-08-11 09:36:40 -0600

    chore: eslint 10 upgrade fixes @W-23474629@

--- src/shared/templates/templates.ts
+++ src/shared/templates/templates.ts
@@ -251,4 +251,4 @@ const getPrecisionTag = (data: CustomField): string =>
 const getScaleTag = (scale: NonNullable<CustomField['scale']>): string =>
   // CustomField thinks this is a number.  The UT had it as a string.
   // This will work for either(filtering out null / undefined because only ==)
-  typeof scale !== null ? `\t<scale>${scale}</scale>\n` : '';
+  scale != null ? `\t<scale>${scale}</scale>\n` : '';
